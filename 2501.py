95ed5757f44809cb66ab80357545188557070873
usok1ng <jws5271@naver.com>
2023-03-06 16:10:11 +0900

    2501

---
+++ 2501.py
@@ -0,0 +1,15 @@
+# [#3] Bronze3 2501 약수 구하기
+
+a, b = map(int, input().split())
+arr = [0]
+cnt = 0
+
+for i in range(1, a):
+    if a % i == 0:
+        arr.append(i)
+        cnt += 1
+
+if b > cnt:
+    print(0)
+else:
+    print(arr[b])
